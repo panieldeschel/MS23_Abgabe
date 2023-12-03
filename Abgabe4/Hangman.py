74259f8dae6a3983ea94dfbb85a77e474f0298b6
panieldeschel <dyehttodptw@gmail.com>
2023-12-03 13:44:11 +0100

    Added Abgabe4

---
+++ Abgabe4/Hangman.py
@@ -0,0 +1,28 @@
+# Play Hangman inside the console
+
+import os
+import WordGuesser
+import keyboard
+
+wordGuesser = WordGuesser.WordGuesser()
+
+# loop the game until the word is completed
+while True:
+    # reset canvas and write UI
+    os.system('cls')                            # only works on Windows  
+    print('''
+        Hangman, ohne jemanden zu Hängen - Viel Spaß!
+        Zum Raten bitte einen Buchstaben eintippen:
+
+        %s
+    ''' % wordGuesser.getProgressString())
+
+    # if word is incomplete wait for the next guess
+    if not wordGuesser.isFinished():
+        keyPressed = keyboard.read_key()        # stops code until key is pressed
+        wordGuesser.letterGuessed(keyPressed)
+    # else end the game
+    else:
+        break
+
+print("Du hast es erraten, nicht schlecht!")
