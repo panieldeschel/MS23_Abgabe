6627a7a70776b0a97134eba6f9875e2fa9005147
panieldeschel <dyehttodptw@gmail.com>
2024-01-04 17:35:57 +0100

    renamed Moinify function accordingly

--- Abgabe7/main.py
+++ Abgabe7/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def read_root():
     return {"Moin": "Welt"}
 
-@app.post("/modify-text")
+@app.post("/moinify_text")
 async def moinify_text(text: str):
     moinified_text = moinify(text)
     return {"original_text": text, "moinified_text": moinified_text}
