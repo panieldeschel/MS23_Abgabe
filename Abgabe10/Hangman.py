24d26efce9ceb7986c7a5ac5f2bdf41617d8d06c
panieldeschel <dyehttodptw@gmail.com>
2024-01-14 15:20:11 +0100

    Hangman now Functional Programming

--- Abgabe10/Hangman.py
+++ Abgabe10/Hangman.py
@@ -4,25 +4,22 @@ import os
 import WordGuesser
 import keyboard
 
-wordGuesser = WordGuesser.WordGuesser()
-
-# loop the game until the word is completed
-while True:
-    # reset canvas and write UI
+def gameLoop(progress, target):
     os.system('cls')                            # only works on Windows  
     print('''
         Hangman, ohne jemanden zu Hängen - Viel Spaß!
         Zum Raten bitte einen Buchstaben eintippen:
 
         %s
-    ''' % wordGuesser.getProgressString())
+    ''' % progress)
 
-    # if word is incomplete wait for the next guess
-    if not wordGuesser.isFinished():
-        keyPressed = keyboard.read_key()        # stops code until key is pressed
-        wordGuesser.letterGuessed(keyPressed)
-    # else end the game
+    if progress == target:
+        print("Du hast es erraten, nicht schlecht!")
     else:
-        break
+        # stops code until key is pressed
+        new_progress: Final = WordGuesser.guessingLetter(keyboard.read_key(), progress, target)
+        gameLoop(new_progress, target)
 
-print("Du hast es erraten, nicht schlecht!")
+# loop the game until the word is completed
+wordToGuess = WordGuesser.pickWord('words.json') 
+gameLoop(WordGuesser.hideWord(wordToGuess), wordToGuess)
