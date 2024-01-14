97b965a2f975014096d25fdaa2c1a194f6a080de
panieldeschel <dyehttodptw@gmail.com>
2024-01-14 12:06:42 +0100

    Abgabe10 FP1 Initial Commit

---
+++ Abgabe10/WordGuesser.py
@@ -0,0 +1,46 @@
+import JSONUtils      
+import random 
+import re       
+
+class WordGuesser:
+    def __init__(self):
+        # get a word and hide it
+        self.word = self.initWord()                           # word to guess
+        self.progress = self.initProgressString(self.word)    # progress of guessing
+
+    def initWord(self):
+        # available words are listed in "./words.json"
+        available_words = JSONUtils.readList('words.json')
+        # pick a random available word
+        return random.choice(available_words)
+
+    def initProgressString(self, word):
+        # replace every letter with _ using RegExs
+        hidden_word = re.sub(r'\w', '_', word)
+        return hidden_word
+
+
+
+    # Get Properties
+    def getProgressString(self):
+        return self.progress
+ 
+    def isFinished(self):
+        if self.progress == self.word:
+            return True
+        else:
+            return False
+
+
+
+    # Write Letter
+    def letterGuessed(self, letter):
+        # revert every _ that hides the guessed letter
+        for num, character in enumerate(self.word):
+            if character.lower() == letter:
+                # replace the _ in the middle of the string that hides the correctly guessed letter
+                # keep the old progress before and after that character
+                self.progress = self.progress[:num] + self.word[num] + self.progress[num + 1:]
+
+
+        
